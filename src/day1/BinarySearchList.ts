c89d4deeb9dcc6797b2a584000abb3cb26b59c81
Pablo Medina <jmedinamulet@gmail.com>
2024-01-31 17:11:48 -0500

    Implement binary search algorithm

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,17 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-
+    let high = haystack.length
+    let low = 0
+    
+    while (low < high){
+        let mid = Math.floor(low + (high - low)/2)
+        if (haystack[mid] === needle) {
+            return true
+        }
+        if (haystack[mid] > needle){
+            high = mid
+        }else{
+            low = mid + 1
+        }
+    }
+    return false
 }
