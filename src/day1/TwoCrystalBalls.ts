7712fdf9857d4067a4652c6210d3fe0082a50fb7
Pablo Medina <jmedinamulet@gmail.com>
2024-01-31 18:01:08 -0500

    two crystal balls algo implemented

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,21 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
 
+    const jumpSize = Math.floor(Math.sqrt(breaks.length))
+
+    let i = jumpSize
+    for (; i < breaks.length; i += jumpSize){
+        if(breaks[i]){
+            break;
+        }
+    }
+    
+    i = i - jumpSize
+
+    for (; i < breaks.length; i ++){
+        if ( breaks[i]){
+            return i
+        }
+    }
+
+    return -1
 }
