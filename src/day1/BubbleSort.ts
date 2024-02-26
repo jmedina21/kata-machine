c2d4a1207900545a8a604a3b9206c20cfc70f8ac
Pablo Medina <jmedinamulet@gmail.com>
2024-02-26 16:15:58 -0500

    Implement Bubble Sort and Queue data structure

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,13 @@
 export default function bubble_sort(arr: number[]): void {
-
-}
+    let count = arr.length
+    while (count >= 1){
+        for (let i = 0; i < count; i ++){
+            let current = arr[i]
+            if(current > arr[i + 1]){
+                arr[i] = arr[i + 1]
+                arr[i + 1] = current
+            }
+        }
+        count --
+    }
+} 
